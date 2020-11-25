d59d4e655bf45395db36da8a012181dc5e2022be
Student <Student@ML-RefVm-190432>
2020-11-25 15:17:34 -0600

    adding views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace KurthProject2Vet.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
     }
 }
